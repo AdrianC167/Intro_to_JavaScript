f40ef507ac4e3fef6f7a753b43c6eb69e696361f
Adrian Chen <achen@brooklynsteamcenter.org>
2023-04-17 14:53:12 -0400

    made hello.js and grade_converter.js with js

---
+++ grade_converter.js
@@ -0,0 +1,19 @@
+let grade = "fifty five"
+
+if (grade >= 101) {
+console.log("You have a A+")    
+} else if (grade >= 89) {
+console.log("You have an A")
+} else if (grade >= 79) {
+console.log("You have a B")
+}else if (grade >= 69) {
+console.log("You have a C")
+} else if (grade >= 59) {
+console.log("You have a D")
+} else if (grade >= 0) {
+console.log("You failed with a F")
+} else if (grade <=0) {
+console.log("You should just expell yourself...")
+}
+
+
--- hello.js
+++ hello.js
@@ -1,1 +1,12 @@
 console.log("Hello World")
+let myName = "Adrian"
+let myAge = 160
+console.log("My Name is " + myName + " and im " + myAge + " years old")
+
+if (myAge == 160) {console.log("you are 160")}  else if (myAge >= 18) { console.log("Your of age legally")}  else {console.log("your under 18")}
+
+let myList = [1 , 2 , 3 , 4 , 5 , 6]
+
+for (let numbers of myList) {
+console.log(numbers * 27)
+}
