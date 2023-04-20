6d1364dc178045d015f93314d607d023f965c41a
Adrian Chen <achen@brooklynsteamcenter.org>
2023-04-20 14:06:15 -0400

    made a bad beanstalk for you to walk on

--- turtles.js
+++ turtles.js
@@ -2,6 +2,27 @@ let aquarium = document.getElementById("aquarium")
 
 let turtle = new RealTurtle.default(aquarium, {autoStart: true})
 
+
+turtle.setSpeed(1)
+
+turtle.setPosition(0, 0)
+turtle.setFillStyle("lightblue")
+turtle.setStrokeColorRGB(255,255,255)
+
+turtle.beginPath()
+turtle.right(90)
+turtle.forward(196)
+turtle.right(90)
+turtle.forward(123)
+turtle.right(90)
+turtle.forward(196)
+turtle.right(90)
+turtle.forward(123)
+turtle.closePath()
+turtle.fill()
+
+
+
 turtle.setStrokeColorRGB(213,123,232)
 turtle.setFillStyle("lightgrey")
 
@@ -10,32 +31,33 @@ turtle.setFillStyle("lightgrey")
 turtle.arc(0, 0)
 
 
-
 turtle.setPosition(60, 120)
 
 turtle.beginPath()
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < 2; i++) {
+    turtle.forward(85)
+    turtle.right(90)
     turtle.forward(75)
     turtle.right(90)
 }
 turtle.closePath()
 turtle.fill()
 
-turtle.forward(75)
+turtle.forward(85)
 
 turtle.setFillStyle("black")
 turtle.beginPath()
 
-turtle.right(45)
-turtle.forward(55)
-turtle.right(91)
-turtle.forward(55)
-turtle.right(45)
+turtle.right(60)
+turtle.forward(43)
+turtle.right(60)
+turtle.forward(43)
+turtle.right(60)
 
 turtle.closePath()
 turtle.fill()
 
-turtle.forward(75)
+turtle.forward(85)
 
 
 turtle.right(90)
@@ -79,7 +101,7 @@ turtle.fill()
 
 
 
-turtle.setPosition(15, 30)
+turtle.setPosition(22, 40)
 turtle.setFillStyle("yellow")
 
 
@@ -118,7 +140,7 @@ turtle.forward(77)
 turtle.right(90)
 
 turtle.beginPath()
-for (let i = 0; i < 18; i++) {
+for (let i = 0; i < 20; i++) {
     turtle.forward(4)
     turtle.right(180)
     turtle.forward(4)
@@ -126,6 +148,115 @@ for (let i = 0; i < 18; i++) {
     turtle.forward(3)
     turtle.right(90)
 }
+
+turtle.closePath
+
+turtle.setPosition(180, 120)
+turtle.beginPath()
+for (let i = 0; i < 4; i++) {
+        turtle.forward(15)
+        turtle.right(90)
+        turtle.forward(1)
+        turtle.right(90)
+        turtle.forward(15)
+        turtle.left(90)
+        turtle.forward(1)
+        turtle.left(90)
+    }
+
+turtle.forward(15)
+
+    
+for (let i = 0; i < 4; i++) {
+        turtle.forward(15)
+        turtle.right(90)
+        turtle.forward(1)
+        turtle.right(90)
+        turtle.forward(15)
+        turtle.left(90)
+        turtle.forward(1)
+        turtle.left(90)
+}
+
+
+
+turtle.forward(15)
+
+for (let i = 0; i < 4; i++) {
+    turtle.forward(15)
+    turtle.right(90)
+    turtle.forward(1)
+    turtle.right(90)
+    turtle.forward(15)
+    turtle.left(90)
+    turtle.forward(1)
+    turtle.left(90)
+}
+
+turtle.forward(15)
+
+for (let i = 0; i < 4; i++) {
+    turtle.forward(15)
+    turtle.right(90)
+    turtle.forward(1)
+    turtle.right(90)
+    turtle.forward(15)
+    turtle.left(90)
+    turtle.forward(1)
+    turtle.left(90)
+}
+
+turtle.forward(15)
+
+for (let i = 0; i < 4; i++) {
+    turtle.forward(15)
+    turtle.right(90)
+    turtle.forward(1)
+    turtle.right(90)
+    turtle.forward(15)
+    turtle.left(90)
+    turtle.forward(1)
+    turtle.left(90)
+}
+
+turtle.forward(15)
+
+for (let i = 0; i < 4; i++) {
+    turtle.forward(15)
+    turtle.right(90)
+    turtle.forward(1)
+    turtle.right(90)
+    turtle.forward(15)
+    turtle.left(90)
+    turtle.forward(1)
+    turtle.left(90)
+}
+turtle.forward(15)
+
+for (let i = 0; i < 4; i++) {
+    turtle.forward(15)
+    turtle.right(90)
+    turtle.forward(1)
+    turtle.right(90)
+    turtle.forward(15)
+    turtle.left(90)
+    turtle.forward(1)
+    turtle.left(90)
+}
+
+turtle.forward(15)
+
+for (let i = 0; i < 4; i++) {
+    turtle.forward(15)
+    turtle.right(90)
+    turtle.forward(1)
+    turtle.right(90)
+    turtle.forward(15)
+    turtle.left(90)
+    turtle.forward(1)
+    turtle.left(90)
+}
+
 
 turtle.closePath
 
